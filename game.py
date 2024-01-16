0cf0af5382b92381fc145d7b7c3387e1a32bff15
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-01-16 16:49:39 -0300

    Adding fight turn

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 class Character:
   def __init__(self, name, life, level):
     self.__name = name
@@ -13,8 +14,19 @@ class Character:
   def get_level(self):
     return self.__level
   
-  def details(self):
-    return f"Name: {self.get_name()} \n Life: {self.get_life()} \n Leve: {self.get_level()}"
+  def show_details(self):
+    return f"Name: {self.get_name()} \n Life: {self.get_life()} \n Level: {self.get_level()}"
+  
+  def reduce_life(self, damage):
+    self.__life -= damage
+    if self.__life < 0:
+      self.__live = 0
+
+  def attack(self, target):
+    damage = random.randint(self.get_level() * 2, self.get_level() * 4)
+    target.reduce_life(damage)
+    print(f"{self.get_name()} has attacked {target.get_name()} and caused {damage} damage.")
+
   
 class Hero(Character):
   def __init__(self, name, life, level, special_abilities):
@@ -24,8 +36,13 @@ class Hero(Character):
   def get_special_abilities(self):
     return self.__special_abilities
   
-  def details(self):
-    return f"{super().details()} \nSpecial Abilities: {self.get_special_abilities()} \n"
+  def show_details(self):
+    return f"{super().show_details()} \nSpecial Abilities: {self.get_special_abilities()} \n"
+  
+  def special_attack(self, target):
+    damage = random.randint(self.get_level() * 5, self.get_level() * 8)
+    target.reduce_life(damage)
+    print(f"{self.get_name()} has use an speccial attack {self.get_special_abilities()} on {target.get_name()} and caused {damage} damage.")
   
 class Enemy(Character):
   def __init__(self, name, life, level, kind):
@@ -35,12 +52,43 @@ class Enemy(Character):
   def get_kind(self):
     return self.__Kind
   
-  def details(self):
-    return f"{super().details()} \n Kind: {self.get_kind()} \n"
+  def show_details(self):
+    return f"{super().show_details()} \n Kind: {self.get_kind()} \n"
 
 
-hero_1 = Hero(name="John Doe", life=100, level=5, special_abilities="Blizzard")
-enemy_1 = Enemy(name="Dragon", life=70, level=3, kind="Fire")
+class Game:
+  def __init__(self) -> None:
+    self.hero = Hero(name="John Doe", life=100, level=5, special_abilities="Blizzard")
+    self.enemy = Enemy(name="Dragon", life=90, level=4, kind="Fire")
 
-print(hero_1.details())
-print(enemy_1.details())
+
+  def start_battle(self):
+    print("Starting battle: ")
+    while self.hero.get_life() > 0 and self.enemy.get_life() > 0:
+      print("\n Characters details: ")
+      print(self.hero.show_details())
+      print(self.enemy.show_details())
+
+      print("Press Enter to attack...")
+      choice = input("Choose 1 - Normal Attack, 2 - Special Attack: ")
+
+      if choice == '1':
+        self.hero.attack(self.enemy)
+      elif choice == '2':
+        self.hero.special_attack(self.enemy)
+      else:
+        print("Choice not avaialable.")
+
+      if self.enemy.get_life() > 0:
+        print(f"It's the {self.enemy.get_name()} turn.")
+        self.enemy.attack(self.hero)
+
+    if self.hero.get_life() > 0:
+      print(f"\n The enemy {self.enemy.get_name()} has no life anymore.")
+      print(f"\nCongratulations!!! You won the battle.")
+    else:
+      print(f"\n You lost!!!")
+
+game = Game()
+game.start_battle()
+
